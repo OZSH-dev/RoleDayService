d4cb133fb4f1c626b28c36270a225f44dcd40b3c
rx00 <it.tec@me.com>
2018-08-26 01:00:42 +0500

    fix items views

--- shop/views.py
+++ shop/views.py
@@ -15,7 +15,7 @@ def get_already_bought_item_ids_set(user):
 @login_required
 def index(request):
     user_single_items = get_already_bought_item_ids_set(request.user)
-    items_to_sell = Item.objects.filter(~Q(id__in=user_single_items))
+    items_to_sell = Item.objects.filter(~Q(id__in=user_single_items) | ~Q(transaction__state__lt=2))
     return render(request, "shop_page/shop.html", {"items": items_to_sell})
 
 
